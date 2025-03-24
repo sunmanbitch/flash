38a4897f14257c0d8f48300d0d02120d7c324e3d
sunmanbitch <thrift.splice-0x@icloud.com>
2025-03-24 11:51:40 +0800

    hello3 functions

--- functions/hello.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response("Hello from Cloudflare Pages Functions integrated with Vue!", {
-    headers: { "Content-Type": "text/plain" }
-  });
-}
